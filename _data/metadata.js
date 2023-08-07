13ee8cc17507787485a94a2f6b350f59628ff320
OwenTH <o.thomashargreaves@gmail.com>
2023-08-07 15:23:51 +0100

    More edits

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Owen's 11ty Blog",
-	url: "https://example.com/",
+	url: "https://fancy-sunshine-4deefb.netlify.app/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as I study Web Development.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Owen Thomas-Hargreaves",
+		email: "o.thomashargreaves@gmail.com",
+		url: "https://fancy-sunshine-4deefb.netlify.app/about-me/"
 	}
 }
